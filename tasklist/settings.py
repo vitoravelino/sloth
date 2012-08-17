ea3332f2290bdbc02b7152207ab04b489796bd58
Vítor Avelino <contact@vitoravelino.net>
2012-08-17 13:27:11 -0300

    fixed the environment variable to use heroku postgres database correctly

--- tasklist/settings.py
+++ tasklist/settings.py
@@ -1,7 +1,7 @@
 import os
 
 SECRET_KEY = "\x87\xcaO\xbf.o\xe4\xc4\xd9\x18C\x8cc}4Z\xaf:b\t\xbf\xb0I{"
-DB_URL = os.environ.get('DATABASE_URL')
+DB_URL = os.environ.get('HEROKU_POSTGRESQL_GRAY_URL')
 
 if not DB_URL:
     DB_URL = 'sqlite:////tmp/test.db'
